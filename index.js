8aab4b1df5474003433a1a430cc3b54efd1f1d23
Craig <craigyazzie143@icloud.com>
2026-05-07 15:45:45 -0600

    Update index.js
    like 8

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ let school = {
   "average class size": 25,
   mascot: "Eagles"
 };
-document.getElementById("result").innerHTML = `${school.name} has an average class size of ${school["averageClassSize"]} and their mascot is a ${school.mascot}`;
+document.getElementById("result").innerHTML = `${school.name} has an average class size of ${school["average class size"]} and their mascot is a ${school.mascot}`;
