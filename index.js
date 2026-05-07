7d272f2ddbd790b0be8dd94ab1da8a06f3d42d15
Craig <craigyazzie143@icloud.com>
2026-05-07 15:34:45 -0600

    Update index.js
    gg

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+let school = {
+  name: "My School",
+  averageClassSize: 25,
+  mascot: "Eagles"
+};
+document.getElementById("result").innerHTML = `${school.name} has an average class size of ${school.averageClassSize} and their mascot is a ${school.mascot}`;
